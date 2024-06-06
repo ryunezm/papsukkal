d0c56fcf103e5dc308ae2d6aa78840bc49b39e56
ryunezm <61928963+ryunezm@users.noreply.github.com>
2024-06-05 20:03:09 -0500

    First commit.

---
+++ src/main/java/com/ryunezm/papsukkal/PapsukkalApplication.java
@@ -0,0 +1,13 @@
+package com.ryunezm.papsukkal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapsukkalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PapsukkalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ryunezm/papsukkal/PapsukkalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryunezm.papsukkal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapsukkalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
