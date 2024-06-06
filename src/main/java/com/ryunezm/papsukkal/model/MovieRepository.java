1e7c5d3eecb50ba90464a6ab016f54526d8873d2
ryunezm <61928963+ryunezm@users.noreply.github.com>
2024-06-06 17:43:53 -0500

    Added queries.

--- src/main/java/com/ryunezm/papsukkal/model/MovieRepository.java
+++ src/main/java/com/ryunezm/papsukkal/model/MovieRepository.java
@@ -1,6 +1,27 @@
 package com.ryunezm.papsukkal.model;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface MovieRepository extends MongoRepository<Movie, String> {
+
+    @Query("{$or:[" +
+            "{'directedBy': {$regex: ?0, $options: 'i'}}," +
+            "{'screenplayBy': {$regex: ?0, $options: 'i'}}," +
+            "{'producedBy': {$regex: ?0, $options: 'i'}}," +
+            "{'starring': {$regex: ?0, $options: 'i'}}," +
+            "{'cinematography':  {$regex: ?0, $options: 'i'}}," +
+            "{'editedBy':  {$regex: ?0, $options:  'i'}}," +
+            "{'musicBy':  {$regex: ?0, $options:  'i'}}," +
+            "]}")
+    List<Movie> findPersonByAnyRol(String keyword);
+
+    @Query("{$or:[" +
+            "{'titleOriginal': {$regex: ?0, $options: 'i'}}," +
+            "{'titleEN': {$regex: ?0, $options: 'i'}}," +
+            "{'titleES': {$regex: ?0, $options: 'i'}}" +
+            "]}")
+    List<Movie> findByAnyTitle(String title);
 }
