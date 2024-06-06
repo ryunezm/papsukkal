36305604f93655709c63e20b75e9bab7e574f78c
ryunezm <61928963+ryunezm@users.noreply.github.com>
2024-06-06 12:40:41 -0500

    Removed entity "Person" to keep it simple.

---
+++ src/main/java/com/ryunezm/papsukkal/model/Movie.java
@@ -0,0 +1,32 @@
+    package com.ryunezm.papsukkal.model;
+
+
+    import lombok.Data;
+    import org.springframework.data.annotation.Id;
+    import org.springframework.data.mongodb.core.mapping.Document;
+
+    import java.time.LocalDate;
+    import java.util.List;
+
+    @Data
+    @Document(collection = "movies")
+    public class Movie {
+
+        @Id
+        private String id;
+        private String titleOriginal;
+        private String titleEN;
+        private String titleES;
+        private List<String> directedBy;
+        private List<String> screenplayBy;
+        private List<String> producedBy;
+        private List<String> starring;
+        private List<String> cinematography;
+        private List<String> editedBy;
+        private List<String> musicBy;
+        private LocalDate releaseDate;
+        private List<Genre> genre;
+        private List<SubGenre> subgenre;
+        private int runningTime;
+        private List<Language> language;
+    }
