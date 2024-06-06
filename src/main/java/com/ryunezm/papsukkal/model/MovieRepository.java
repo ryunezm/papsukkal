228f47210be50335a9e02ab37ed11b53ad978831
ryunezm <61928963+ryunezm@users.noreply.github.com>
2024-06-06 12:53:20 -0500

    Reformated code.

--- src/main/java/com/ryunezm/papsukkal/model/MovieRepository.java
+++ src/main/java/com/ryunezm/papsukkal/model/MovieRepository.java
@@ -2,5 +2,5 @@ package com.ryunezm.papsukkal.model;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface MovieRepository extends MongoRepository <Movie, String> {
+public interface MovieRepository extends MongoRepository<Movie, String> {
 }
