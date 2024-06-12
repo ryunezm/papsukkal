f79a890fbf76e4173dbee6e94e604eb54bcc91ff
ryunezm <61928963+ryunezm@users.noreply.github.com>
2024-06-12 17:13:41 -0500

    Some comments, not important.

--- movie-app/src/app/app.component.ts
+++ movie-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {MovieService} from "./movie.service";
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,36 @@ import {RouterOutlet} from '@angular/router';
 })
 export class AppComponent {
   title = 'movie-app';
+  // movie: MovieService = {
+  //   title: "",
+  //   releaseDate: "",
+  //   genre: [],
+  //   runningTime: 0,
+  //   country: [],
+  //   language: []
+  // };
+  //
+  // constructor(private movieService: MovieService) {}
+  //
+  // saveMovie() {
+  //   if (this.movie.id) {
+  //     this.movieService.updateMovie(this.movie.id, this.movie).subscribe(
+  //       response => {
+  //         console.log('Response from server:', response);
+  //       },
+  //       error => {
+  //         console.error('Error:', error);
+  //       }
+  //     );
+  //   } else {
+  //     this.movieService.createMovie(this.movie).subscribe(
+  //       response => {
+  //         console.log('Response from server:', response);
+  //       },
+  //       error => {
+  //         console.error('Error:', error);
+  //       }
+  //     );
+  //   }
+  // }
 }
