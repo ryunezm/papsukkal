3269bd158089e0c28ec140e3d550e8aded2bd273
ryunezm <61928963+ryunezm@users.noreply.github.com>
2024-06-11 19:34:47 -0500

    Added basic functionality.

--- movie-app/src/app/app.routes.ts
+++ movie-app/src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {MovieListComponent} from "./movie-list/movie-list.component";
+import {MovieFormComponent} from "./movie-form/movie-form.component";
+import {MovieDetailComponent} from "./movie-detail/movie-detail.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'movies', pathMatch: 'full'},
+  {path: 'movies', component: MovieListComponent},
+  {path: 'movies/new', component: MovieFormComponent},
+  {path: 'movies/:id', component: MovieDetailComponent},
+  {path: 'movies/edit/:id', component: MovieFormComponent},
+];
--- movie-app/src/app/movie-detail/movie-detail.component.ts
+++ movie-app/src/app/movie-detail/movie-detail.component.ts
@@ -1,18 +1,20 @@
 import {Component} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router, RouterLink} from '@angular/router';
 import {MovieService} from "../movie.service";
 
 @Component({
   selector: 'app-movie-detail',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './movie-detail.component.html',
   styleUrl: './movie-detail.component.scss'
 })
 export class MovieDetailComponent {
   movie: any;
 
-  constructor(private route: ActivatedRoute, private movieService: MovieService) {}
+  constructor(private route: ActivatedRoute, private movieService: MovieService, private router: Router) {}
 
   ngOnInit(): void{
     const id = this.route.snapshot.paramMap.get('id');
@@ -20,6 +22,13 @@ export class MovieDetailComponent {
       this.movieService.getMovieById(id).subscribe(data => {
         this.movie = data;
       })
+    }
+  }
+
+  deleteMovie(): void{
+    const id = this.movie.id;
+    if (id){
+      this.movieService.deleteMovie(id).subscribe(()=>{this.router.navigate(['/movies']).then(() => {})})
     }
   }
 }
