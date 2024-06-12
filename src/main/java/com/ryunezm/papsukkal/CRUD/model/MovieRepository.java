1a30a9112d6972671571c619105507dbf6f19303
ryunezm <61928963+ryunezm@users.noreply.github.com>
2024-06-12 17:15:00 -0500

    Cleaning code.

--- src/main/java/com/ryunezm/papsukkal/CRUD/model/MovieRepository.java
+++ src/main/java/com/ryunezm/papsukkal/CRUD/model/MovieRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.mongodb.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 public interface MovieRepository extends MongoRepository<Movie, String> {
 
--- src/main/java/com/ryunezm/papsukkal/global/exceptions/GlobalException.java
+++ src/main/java/com/ryunezm/papsukkal/global/exceptions/GlobalException.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
