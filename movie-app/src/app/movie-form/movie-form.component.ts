0c4d8d4b61374f685f4096664ff1dbba8d085092
ryunezm <61928963+ryunezm@users.noreply.github.com>
2024-06-21 18:42:22 -0500

    Now it'll send arrays in JSON.

--- movie-app/src/app/movie-form/movie-form.component.ts
+++ movie-app/src/app/movie-form/movie-form.component.ts
@@ -20,19 +20,27 @@ import {Language} from '../enums/language.enum';
 export class MovieFormComponent implements OnInit {
   movie: any = {
     directedBy: [],
+    directedByString: '',
     screenplayBy: [],
+    screenplayByString: '',
     producedBy: [],
+    producedByString: '',
     starring: [],
+    starringString: '',
     cinematography: [],
+    cinematographyString: '',
     editedBy: [],
+    editedByString: '',
     musicBy: [],
+    musicByString: '',
     productionCompany: [],
+    productionCompanyString: '',
     genres: [],
     subgenres: [],
     countries: [],
     languages: [],
     personalRating: {
-      screenplay:0,
+      screenplay: 0,
       acting: 0,
       photography: 0,
       entertainment: 0,
@@ -61,6 +69,14 @@ export class MovieFormComponent implements OnInit {
     if (id) {
       this.movieService.getMovieById(id).subscribe(data => {
         this.movie = data;
+        this.movie.directedByString = this.movie.directedBy.join(';');
+        this.movie.screenplayByString = this.movie.screenplayBy.join(';');
+        this.movie.producedByString = this.movie.producedBy.join(';');
+        this.movie.starringString = this.movie.starring.join(';');
+        this.movie.cinematographyString = this.movie.cinematography.join(';');
+        this.movie.editedByString = this.movie.editedBy.join(';');
+        this.movie.musicByString = this.movie.musicBy.join(';');
+        this.movie.productionCompanyString = this.movie.productionCompany.join(';');
         this.initializePersonalRating();
         this.onGenresChange();
       });
@@ -69,20 +85,34 @@ export class MovieFormComponent implements OnInit {
     }
   }
 
-  onFieldChange(fieldName: string, value: any): void {
-    console.log(`${fieldName} changed:`, value);
-  }
-
-
   saveMovie(): void {
     this.subgenreValidatorService.validateSubgenres(this.movie.subgenres, this.movie.genres);
+    this.movie.directedBy = this.movie.directedByString.split(';').map((item: string) => item.trim());
+
+    const fieldsToSplit = [
+      'directedBy',
+      'screenplayBy',
+      'producedBy',
+      'starring',
+      'cinematography',
+      'editedBy',
+      'musicBy',
+      'productionCompany'
+    ];
+
+    fieldsToSplit.forEach(field => {
+      this.movie[field] = (this.movie[`${field}String`] || '').split(';').map((item: string) => item.trim());
+    });
+
     if (this.movie.id) {
       this.movieService.updateMovie(this.movie.id, this.movie).subscribe(() => {
-        this.router.navigate(['/movies']).then(r => {});
+        this.router.navigate(['/movies']).then(r => {
+        });
       });
     } else {
       this.movieService.createMovie(this.movie).subscribe(() => {
-        this.router.navigate(['/movies']).then(r => {});
+        this.router.navigate(['/movies']).then(r => {
+        });
       });
     }
   }
@@ -110,9 +140,17 @@ export class MovieFormComponent implements OnInit {
       }
     }
 
+    console.log(`${type} current values:`, this.movie[type]);
+
     if (type === 'genres') {
       this.onGenresChange();
     }
+  }
+
+  onFieldChange(fieldName: string, value: any): void {
+    const arrayValue = value.split(';').map((item: string) => item.trim());
+    this.movie[fieldName] = Array.from(new Set(arrayValue));
+    console.log(`${fieldName} array:`, arrayValue);
   }
 
   validateNumberInput(event: KeyboardEvent): void {
