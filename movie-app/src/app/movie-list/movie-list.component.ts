5b82811b1a276b6db8ab4bd14f8a0c95673327a0
ryunezm <61928963+ryunezm@users.noreply.github.com>
2024-07-13 17:41:08 -0500

    Added a search bar and a table to list all movies.

--- movie-app/src/app/movie-list/movie-list.component.ts
+++ movie-app/src/app/movie-list/movie-list.component.ts
@@ -15,14 +15,37 @@ import {MatButton} from "@angular/material/button";
 })
 export class MovieListComponent {
   movies: any[] = [];
+  hoveredMovieId: number | null = null;
+  animatingMovieId: number | null = null;
+  filteredMovies: any[] = [];
 
-  constructor(private movieService: MovieService) {
-  }
+  constructor(private movieService: MovieService) {}
 
   ngOnInit(): void {
     this.movieService.getMovies().subscribe(data => {
       this.movies = data;
-    })
+      this.filteredMovies = [...this.movies];
+    });
   }
 
+  onSearchChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const value = target.value;
+    if (!value) {
+      this.filteredMovies = [...this.movies];
+      return;
+    }
+    this.filteredMovies = this.movies.filter(movie =>
+      movie.title.toLowerCase().includes(value.toLowerCase())
+    );
+  }
+  formatArray(array: string[]): string {
+    return array.join(' / ');
+  }
+
+  onMouseEnter(movieId: number) { this.hoveredMovieId = movieId; }
+
+  onMouseLeave() { this.hoveredMovieId = null; }
+
+  onAnimationEnd(movieId: number) { if (this.hoveredMovieId !== movieId) { this.animatingMovieId = null; } }
 }
