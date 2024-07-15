9700bbbbd0aa774daa31116381e01d6709e2a8e7
ryunezm <61928963+ryunezm@users.noreply.github.com>
2024-07-14 23:27:41 -0500

    Cleaning code.

--- movie-app/src/app/app.component.ts
+++ movie-app/src/app/app.component.ts
@@ -1,46 +1,14 @@
 import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {MovieService} from "./movie.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'movie-app';
-  // movie: MovieService = {
-  //   title: "",
-  //   releaseDate: "",
-  //   genre: [],
-  //   runningTime: 0,
-  //   country: [],
-  //   language: []
-  // };
-  //
-  // constructor(private movieService: MovieService) {}
-  //
-  // saveMovie() {
-  //   if (this.movie.id) {
-  //     this.movieService.updateMovie(this.movie.id, this.movie).subscribe(
-  //       response => {
-  //         console.log('Response from server:', response);
-  //       },
-  //       error => {
-  //         console.error('Error:', error);
-  //       }
-  //     );
-  //   } else {
-  //     this.movieService.createMovie(this.movie).subscribe(
-  //       response => {
-  //         console.log('Response from server:', response);
-  //       },
-  //       error => {
-  //         console.error('Error:', error);
-  //       }
-  //     );
-  //   }
-  // }
 }
